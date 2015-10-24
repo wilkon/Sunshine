c17bd97ec5babd9104e44128c183ba8470dfc0ce
willis <tb.wilkon@gmail.com>
2015-10-24 17:34:27 -0400

    Merge branch 'master' of https://github.com/wilkon/Sunshine
    Conflicts:
    	app/src/main/java/com/example/android/sunshine/data/WeatherContract.java
    	app/src/main/java/com/example/android/sunshine/data/WeatherDbHelper.java

--- app/src/main/java/com/example/android/sunshine/data/WeatherDbHelper.java
+++ app/src/main/java/com/example/android/sunshine/data/WeatherDbHelper.java
@@ -19,8 +19,6 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import com.example.android.sunshine.app.data.WeatherContract.LocationEntry;
-import com.example.android.sunshine.app.data.WeatherContract.WeatherEntry;
 import com.example.android.sunshine.data.WeatherContract.*;
 
 
